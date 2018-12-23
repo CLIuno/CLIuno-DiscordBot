4896ce7c2324fa6b0bd52b40c0e2195acea25976
RMAH x HOOK <albraa142099@gmail.com>
2018-12-23 18:10:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,73 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const PREFIX = ""
+const bot = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+var fortunes = [
+    "اترزع زاكر",
+    "شباب لازم تعلموا براكتس",
+    "انشر السيرفر "
+];
+
+var fsing = [
+    "I am faaaaaaded",
+    "laaaaa laaaa leeeee",
+    "I have bad voice",
+    "not now",
+    "Despacito Quiero respirar tu cuello despacito  Deja que te diga cosas al oído Para que te acuerdes si no estás conmigo",
+    "Don't give up, I won't give up"
+    
+];
+
+bot.on("ready", function(){
+    console.log("Ready");
+    bot.user.setGame('尺௱♡Ӈƙ ❤','https://www.twitch.tv/RMxHK');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+bot.on("guildMemberAdd", function(member){
+    member.guild.channels.find("name", "general").sendMessage("yoo guys " + member.toString() + " is here !");
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.on("message", function(message){
+     if (message.author.equals(bot.user)) return;
+                                                                                                                                          
+    if (!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0].toLowerCase()){
+       
+       case "tso":
+       message.channel.sendMessage("yea what u want :)");
+       break;
+
+       case "info":
+       message.channel.sendMessage("I am busy call me later");
+       break;
+          
+       case "نصيحة":
+        if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
+        else message.channel.sendMessage("الرجاء معاودة الاتصال في وقت لاحق");
+
+       break;
+
+       case "sing":
+       if (args[1]) message.channel.sendMessage(fsing[Math.floor(Math.random() * fsing.length)]);
+       else message.channel.sendMessage(fsing[Math.floor(Math.random() * fsing.length)]);
+       break;
+
+       case "embed":
+       var embed = new Discord.RichEmbed()
+            .addField("Hello, This is TSO ", "use -_help for more info")
+            .setColor(0x00FFFF)
+            .setFooter("created by ☲尺௱_♡_Ӈƙ☲®")
+            .setThumbnail(message.author.avatarURL)
+            message.channel.sendMessage(embed);
+       
+       break;
+
+       default:
+       message.channel.sendMessage("invalid command")
+    }
+});
+
+bot.login(process.env.BOT_TOKEN);
