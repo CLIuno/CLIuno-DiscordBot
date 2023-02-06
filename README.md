a5fc62232d34bd02a3124a542eaf89b7ad9092eb
ru44 <18311071+ru44@users.noreply.github.com>
2023-02-06 21:36:17 +0300

    start working on it

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# fcitbot
+# Discord bot For all in one cli project
+
+this bot will help of all in one cli project
+and will give you all the information about the project
+how to use it and how to install it and some example of it
--- bot.js
+++ bot.js
@@ -1,105 +1,53 @@
 const Discord = require("discord.js");
-const PREFIX = "$"
+const PREFIX = "$";
 const bot = new Discord.Client();
 
-var fortunes = [
-   "اعمل براكتس",
-    "اترزع زاكر",
-    "خليك هاوي",
-    "لازم تحب البرمجة",
-    "لا تخلي الهواة احسن منك",
-    "حل تمارين الكتاب"
-];
-
-var fsing = [
-    "I am faaaaaaded",
-    "laaaaa laaaa leeeee",
-    "I have bad voice",
-    "not now",
-    "Despacito Quiero respirar tu cuello despacito  Deja que te diga cosas al oído Para que te acuerdes si no estás conmigo",
-    "Don't give up, I won't give up"
-    
-];
-
-var df = [
-    "روح عني أنا تعبان",
-    "أفصخلك يعني؟",
-    "انقلع اطلع برا",
-    "كلمني بعدين دحين جالس اتفرج مسلسل",
-    "كلم شوبوب يساعدوك",
-    "يبن الووس "
-    
-];
-var dx = [
-    "مخك تعبان",
-    "كأنك زي الكرسي اللي قاعد عليه",
-    "انقلع اطلع برا",
-    "شكلك تبغى ترسب",
-    "خلي الجوال ينفعك",
-    "قوم يا حثالة ذاكر"
-    
-];
-bot.on("ready", function(){
-    console.log("Ready");
-    bot.user.setGame('尺௱♡Ӈƙ ❤','https://www.twitch.tv/mr_rum');
+bot.on("ready", function () {
+  console.log("Ready");
+  bot.user.setGame("尺௱♡Ӈƙ ❤", "https://www.twitch.tv/ru44_y");
 });
 
-bot.on("guildMemberAdd", function(member){
-    member.guild.channels.find("name", "general").sendMessage("yoo guys " + member.toString() + " is here !");
+bot.on("guildMemberAdd", function (member) {
+  member.guild.channels
+    .find("name", "general")
+    .sendMessage("yoo guys " + member.toString() + " is here !");
 });
 
-bot.on("message", function(message){
-     if (message.author.equals(bot.user)) return;
-                                                                                                                                          
-    if (!message.content.startsWith(PREFIX)) return;
+bot.on("message", function (message) {
+  if (message.author.equals(bot.user)) return;
 
-    var args = message.content.substring(PREFIX.length).split(" ");
+  if (!message.content.startsWith(PREFIX)) return;
 
-    switch (args[0].toLowerCase()){
-       
-       case "tso":
-       message.channel.sendMessage("https://discord.gg/7d4tYg2");
-       break;
+  var args = message.content.substring(PREFIX.length).split(" ");
 
-       case "hp":
-        message.channel.sendMessage("sing "+"info "+"نصيحة"+" قول"+" تهزيء"+" tso");
-       break;
-          
-          case "قول":
-       message.channel.sendMessage(message.content.replace('$قول',''));
-       break;
-          case "صورتي":
-       message.channel.sendMessage("https://www.rollingstone.com/wp-content/uploads/2019/11/TaySnub.jpg?resize=900,600&w=450");
-       break;
- 
-   case "نصيحة":
-       if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
-       else message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
-       break;
-          case "تهزيء":
-       if (args[1]) message.channel.sendMessage(dx[Math.floor(Math.random() * dx.length)]);
-       else message.channel.sendMessage(df[Math.floor(Math.random() * df.length)]);
-       break;
+  switch (args[0].toLowerCase()) {
+    case "aio":
+      message.channel.sendMessage("https://discord.gg/jEhTRb7c");
+      break;
 
-       case "sing":
-       if (args[1]) message.channel.sendMessage(fsing[Math.floor(Math.random() * fsing.length)]);
-       else message.channel.sendMessage(fsing[Math.floor(Math.random() * fsing.length)]);
-       break;
+    case "help":
+      message.channel.sendMessage("aio" + " say");
+      break;
 
-       case "info":
-       var embed = new Discord.RichEmbed()
-            .addField("Hello, This is FCIT BOT ", "use $hp for more info")
-            .setColor(0x00FFFF)
-            .setFooter("created by ☲尺௱_♡_Ӈƙ☲®")
-            .setThumbnail(message.author.avatarURL)
-            message.channel.sendMessage(embed);
+    case "say":
+      message.channel.sendMessage(message.content.replace("$say", ""));
+      break;
 
-       break;
-          
+    case "info":
+      var embed = new Discord.RichEmbed()
+        .addField(
+          "Hello, This is Aio cli Bot",
+          "use $help for more information"
+        )
+        .setColor(0x00ffff)
+        .setFooter("created by 尺௱♡Ӈƙ®#5990")
+        .setThumbnail(message.author.avatarURL);
+      message.channel.sendMessage(embed);
+      break;
 
-       default:
-       message.channel.sendMessage(df[Math.floor(Math.random() * df.length)])
-    }
+    default:
+      message.channel.sendMessage("Wrong Command! use $help for more info");
+  }
 });
 
 bot.login(process.env.BOT_TOKEN);
